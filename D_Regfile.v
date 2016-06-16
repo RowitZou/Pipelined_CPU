72b80c772c890056dadab58880b851d7122f05f4
Yicheng Zou <1094074685@qq.com>
2016-06-16 18:00:26 +0800

    Update D_Regfile.v

--- D_Regfile.v
+++ D_Regfile.v
@@ -1,3 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:29:35 05/17/2016 
+// Design Name: 
+// Module Name:    D_RegFile 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 module D_RegFile(
   input clk,
   input [31:0]ins,
